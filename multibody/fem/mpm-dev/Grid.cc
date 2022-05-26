301803444bb9673a22970de70e2baeedc48dddac
Yimin Lin <yimin.lin@tri.global>
2022-05-26 09:07:25 -0700

    add expand index routine

--- multibody/fem/mpm-dev/Grid.cc
+++ multibody/fem/mpm-dev/Grid.cc
@@ -98,9 +98,10 @@ int Grid::Reduce3DIndex(int i, int j, int k) const {
 }
 
 Vector3<int> Grid::Expand1DIndex(int idx) const {
-    return Vector3<int>(idx % num_gridpt_1D_(0),
-                        (idx / num_gridpt_1D_(0)) % num_gridpt_1D_(1),
-                        idx % (num_gridpt_1D_(0)*num_gridpt_1D_(1)));
+    return Vector3<int>(
+            bottom_corner_(0) + idx % num_gridpt_1D_(0),
+            bottom_corner_(1) + (idx / num_gridpt_1D_(0)) % num_gridpt_1D_(1),
+            bottom_corner_(2) + idx / (num_gridpt_1D_(0)*num_gridpt_1D_(1)));
 }
 
 bool Grid::in_index_range(int i, int j, int k) const {
