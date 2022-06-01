0a0f3cfe0969ed7a1cb651c0b38cbf3944648dc9
Yimin Lin <yimin.lin@tri.global>
2022-06-01 13:16:48 -0700

    Style fix

--- multibody/fem/mpm-dev/test/Particles_test.cc
+++ multibody/fem/mpm-dev/test/Particles_test.cc
@@ -1,5 +1,7 @@
 #include "drake/multibody/fem/mpm-dev/Particles.h"
 
+#include <math.h>
+
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/eigen_matrix_compare.h"
@@ -12,8 +14,6 @@ namespace mpm {
 namespace internal {
 namespace {
 
-constexpr double pi = 3.14159265358979323846;
-constexpr double kEps = 1e-14;
 constexpr double TOLERANCE = 1e-10;
 
 GTEST_TEST(ParticlesClassTest, TestAddSetGet) {
@@ -229,85 +229,99 @@ GTEST_TEST(ParticlesClassTest, TestReorder) {
     particles.AddParticle(pos3, vel3, mass3, vol3, F3, stress3);
 
     // Check the original ordering
-    EXPECT_TRUE(CompareMatrices(particles.get_position(0), pos1, kEps));
-    EXPECT_TRUE(CompareMatrices(particles.get_velocity(0), vel1, kEps));
+    EXPECT_TRUE(CompareMatrices(particles.get_position(0), pos1, TOLERANCE));
+    EXPECT_TRUE(CompareMatrices(particles.get_velocity(0), vel1, TOLERANCE));
     EXPECT_EQ(particles.get_mass(0), mass1);
     EXPECT_EQ(particles.get_reference_volume(0), vol1);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(0), F1,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(0), stress1,
-                kEps));
+                TOLERANCE));
 
     EXPECT_TRUE(CompareMatrices(particles.get_position(1), pos2,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_velocity(1), vel2,
-                kEps));
+                TOLERANCE));
     EXPECT_EQ(particles.get_mass(1), mass2);
     EXPECT_EQ(particles.get_reference_volume(1), vol2);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(1), F2,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(1), stress2,
-                kEps));
+                TOLERANCE));
 
     EXPECT_TRUE(CompareMatrices(particles.get_position(2), pos3,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_velocity(2), vel3,
-                kEps));
+                TOLERANCE));
     EXPECT_EQ(particles.get_mass(2), mass3);
     EXPECT_EQ(particles.get_reference_volume(2), vol3);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(2), F3,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(2), stress3,
-                kEps));
+                TOLERANCE));
 
     std::vector<size_t> new_order{2, 0, 1};
     particles.Reorder(new_order);
     EXPECT_TRUE(CompareMatrices(particles.get_position(0), pos3,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_velocity(0), vel3,
-                kEps));
+                TOLERANCE));
     EXPECT_EQ(particles.get_mass(0), mass3);
     EXPECT_EQ(particles.get_reference_volume(0), vol3);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(0), F3,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(0), stress3,
-                kEps));
+                TOLERANCE));
 
     EXPECT_TRUE(CompareMatrices(particles.get_position(1), pos1,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_velocity(1), vel1,
-                kEps));
+                TOLERANCE));
     EXPECT_EQ(particles.get_mass(1), mass1);
     EXPECT_EQ(particles.get_reference_volume(1), vol1);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(1), F1,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(1), stress1,
-                kEps));
+                TOLERANCE));
 
     EXPECT_TRUE(CompareMatrices(particles.get_position(2), pos2,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_velocity(2), vel2,
-                kEps));
+                TOLERANCE));
     EXPECT_EQ(particles.get_mass(2), mass2);
     EXPECT_EQ(particles.get_reference_volume(2), vol2);
     EXPECT_TRUE(CompareMatrices(particles.get_deformation_gradient(2), F2,
-                kEps));
+                TOLERANCE));
     EXPECT_TRUE(CompareMatrices(particles.get_kirchhoff_stress(2), stress2,
-                kEps));
+                TOLERANCE));
 }
 
-GTEST_TEST(ParticlesClassTest, TestAdvectAndUpdateKirchhoffStress) {
-    std::vector<Vector3<double>> positions;
-    std::vector<Vector3<double>> velocities;
-    std::vector<double> masses;
-    std::vector<double> reference_volumes;
-    std::vector<Matrix3<double>> deformation_gradients;
-    std::vector<Matrix3<double>> kirchhoff_stresses;
+GTEST_TEST(ParticlesClassTest, TestAdvect) {
+    Vector3<double> pos1 = {1.0, 2.0, 3.0};
+    Vector3<double> vel1 = {-1.0, -2.0, -3.0};
+    Vector3<double> pos2 = {3.0, -1.0, 6.0};
+    Vector3<double> vel2 = {-9.0, 8.0, -2.0};
+    Particles particles = Particles();
+    particles.AddParticle(pos1, vel1, 1.0, 1.0,
+                          Matrix3<double>::Zero(), Matrix3<double>::Zero());
+    particles.AddParticle(pos2, vel2, 1.0, 1.0,
+                          Matrix3<double>::Zero(), Matrix3<double>::Zero());
+
+    // Advect particles
+    double dt = 0.3;
+    particles.AdvectParticles(dt);
+    EXPECT_TRUE(CompareMatrices(particles.get_position(0),
+                                Vector3<double>(0.7, 1.4, 2.1), TOLERANCE));
+    EXPECT_TRUE(CompareMatrices(particles.get_position(1),
+                                Vector3<double>(0.3, 1.4, 5.4), TOLERANCE));
+}
+
+GTEST_TEST(ParticlesClassTest, TestUpdateKirchhoffStress) {
     CorotatedModel corotated_model = CorotatedModel(5.0, 0.25);
 
     const Matrix3<double> R =
         math::RotationMatrix<double>
-            (math::RollPitchYaw<double>(pi/2.0, pi, 3.0*pi/2.0)).matrix();
+            (math::RollPitchYaw<double>(M_PI/2.0, M_PI, 3.0*M_PI/2.0)).matrix();
     const Matrix3<double> S = (Matrix3<double>() <<
             6, 1, 2,
             1, 4, 1,
@@ -318,31 +332,19 @@ GTEST_TEST(ParticlesClassTest, TestAdvectAndUpdateKirchhoffStress) {
              40, -44,  18680).finished();
     const Matrix3<double> F = R * S;
 
-    Vector3<double> pos1 = {1.0, 2.0, 3.0};
-    Vector3<double> vel1 = {-1.0, -2.0, -3.0};
-    double mass1 = 5.0;
-    double vol1  = 10.0;
     Matrix3<double> F1 = F;
     Matrix3<double> stress1 = Matrix3<double>::Zero();
 
-    Vector3<double> pos2 = {3.0, -1.0, 6.0};
-    Vector3<double> vel2 = {-9.0, 8.0, -2.0};
-    double mass2 = 7.0;
-    double vol2  = 3.0;
     Matrix3<double> F2 = R;
     Matrix3<double> stress2 = Matrix3<double>::Ones();
 
     Particles particles = Particles();
-    particles.AddParticle(pos1, vel1, mass1, vol1, F1, stress1);
-    particles.AddParticle(pos2, vel2, mass2, vol2, F2, stress2);
-
-    // Advect particles
-    double dt = 0.3;
-    particles.AdvectParticles(dt);
-    EXPECT_TRUE(CompareMatrices(particles.get_position(0),
-                                Vector3<double>(0.7, 1.4, 2.1), kEps));
-    EXPECT_TRUE(CompareMatrices(particles.get_position(1),
-                                Vector3<double>(0.3, 1.4, 5.4), kEps));
+    particles.AddParticle(Vector3<double>(0.0, 0.0, 0.0),
+                          Vector3<double>(0.0, 0.0, 0.0),
+                          1.0, 1.0, F1, stress1);
+    particles.AddParticle(Vector3<double>(0.0, 0.0, 0.0),
+                          Vector3<double>(0.0, 0.0, 0.0),
+                          1.0, 1.0, F2, stress2);
 
     // Update Kirchhoff stress
     particles.UpdateKirchhoffStresses(corotated_model);
