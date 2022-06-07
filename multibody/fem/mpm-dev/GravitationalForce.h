4b8178beb01d1b2b1b98dc29c8db70fa84c922d3
Yimin Lin <yimin.lin@tri.global>
2022-06-07 15:21:23 -0700

    Change class name, modify constructors

--- multibody/fem/mpm-dev/GravitationalForce.h
+++ multibody/fem/mpm-dev/GravitationalForce.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <utility>
 #include <vector>
 
 #include "drake/common/eigen_types.h"
@@ -9,16 +10,20 @@ namespace drake {
 namespace multibody {
 namespace mpm {
 
-// A class providing the utitlities to apply external forces to the grid
-class ExternalForces {
+// A class providing the utitlities to apply gravitational forces to the grid
+class GravitationalForce {
  public:
-    ExternalForces() = default;
+    GravitationalForce();
+    explicit GravitationalForce(Vector3<double> g);
 
     // Apply the gravitational forces to grid points. (Equivalently, apply
     // gravitational acceleration to the velocity)
-    // v_new = v_prev + dt*(0, 0, -g), where g = 9.81
+    // v_new = v_prev + dt*gravitational_acceleration
     void ApplyGravitationalForces(double dt, Grid* grid) const;
-};  // class ExternalForces
+
+ private:
+    Vector3<double> gravitational_acceleration_;
+};  // class GravitationalForce
 
 }  // namespace mpm
 }  // namespace multibody
--- multibody/fem/mpm-dev/test/GravitationalForce_test.cc
+++ multibody/fem/mpm-dev/test/GravitationalForce_test.cc
@@ -1,4 +1,4 @@
-#include "drake/multibody/fem/mpm-dev/ExternalForces.h"
+#include "drake/multibody/fem/mpm-dev/GravitationalForce.h"
 
 #include <gtest/gtest.h>
 
@@ -13,7 +13,7 @@ constexpr double TOLERANCE = 1e-10;
 
 namespace {
 
-GTEST_TEST(ExternalForcesTest, TestApplyGravitationalForces) {
+GTEST_TEST(GravitationalForce, TestApplyGravitationalForces) {
     Vector3<int> num_gridpt_1D = {2, 2, 2};
     double h = 1.0;
     Vector3<int> bottom_corner  = {0, 0, 0};
@@ -30,14 +30,34 @@ GTEST_TEST(ExternalForcesTest, TestApplyGravitationalForces) {
                       batch_index_3d(2), mass_i);
     }
 
-    // Apply gravitational force
-    ExternalForces external_forces = ExternalForces();
-    external_forces.ApplyGravitationalForces(dt, &grid);
+    // Apply gravitational force, default
+    GravitationalForce gravitational_force = GravitationalForce();
+    gravitational_force.ApplyGravitationalForces(dt, &grid);
 
     for (const auto& [batch_index_flat, batch_index_3d] : grid.get_indices()) {
         Vector3<double> velocity_exact =
                             batch_index_flat*Vector3<double>(1.0, 1.0, 1.0)
                           + dt*Vector3<double>(0.0, 0.0, -9.81);
+        double mass_exact = batch_index_flat;
+        EXPECT_EQ(mass_exact, grid.get_mass(batch_index_3d(0),
+                                            batch_index_3d(1),
+                                            batch_index_3d(2)));
+        EXPECT_TRUE(CompareMatrices(velocity_exact,
+                                    grid.get_velocity(batch_index_3d(0),
+                                                      batch_index_3d(1),
+                                                      batch_index_3d(2)),
+                                    TOLERANCE));
+    }
+
+    // Apply gravitational force, arbitrary gravitational acceleration
+    Vector3<double> g = Vector3<double>(0.1, -0.3, 0.2);
+    gravitational_force = GravitationalForce(g);
+    gravitational_force.ApplyGravitationalForces(dt, &grid);
+
+    for (const auto& [batch_index_flat, batch_index_3d] : grid.get_indices()) {
+        Vector3<double> velocity_exact =
+                            batch_index_flat*Vector3<double>(1.0, 1.0, 1.0)
+                          + dt*g + dt*Vector3<double>(0.0, 0.0, -9.81);
         double mass_exact = batch_index_flat;
         EXPECT_EQ(mass_exact, grid.get_mass(batch_index_3d(0),
                                             batch_index_3d(1),
