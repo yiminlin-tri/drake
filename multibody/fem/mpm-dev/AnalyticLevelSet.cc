e76602e2a3f755fbb97ccc9efc2cb00397212262
Yimin Lin <yimin.lin@tri.global>
2022-06-22 14:07:18 -0700

    Add half space level set, InInterior includes boundary

--- multibody/fem/mpm-dev/AnalyticLevelSet.cc
+++ multibody/fem/mpm-dev/AnalyticLevelSet.cc
@@ -17,6 +17,29 @@ const std::array<Vector3<double>, 2>& AnalyticLevelSet::get_bounding_box()
     return bounding_box_;
 }
 
+HalfSpaceLevelSet::HalfSpaceLevelSet(const Vector3<double>& normal):
+                AnalyticLevelSet(std::numeric_limits<double>::infinity(),
+                                 {{-std::numeric_limits<double>::infinity()
+                                    *Vector3<double>::Ones(),
+                                    std::numeric_limits<double>::infinity()
+                                    *Vector3<double>::Ones()}}),
+                normal_(normal.normalized()) {
+    DRAKE_DEMAND(!(normal(0) == 0 && normal(1) == 0 && normal(2) == 0));
+}
+
+bool HalfSpaceLevelSet::InInterior(const Vector3<double>& position) const {
+    return (position.dot(normal_) >= 0.0);
+}
+
+Vector3<double> HalfSpaceLevelSet::Normal(const Vector3<double>& position)
+                                                                        const {
+    if (InInterior(position)) {
+        return -normal_;
+    } else {
+        throw std::logic_error("Normal outside of the level set is unavaiable");
+    }
+}
+
 SphereLevelSet::SphereLevelSet(double radius):
                     AnalyticLevelSet(4.0/3.0*M_PI*radius*radius*radius,
                                     {{-radius*Vector3<double>::Ones(),
@@ -26,7 +49,7 @@ SphereLevelSet::SphereLevelSet(double radius):
 }
 
 bool SphereLevelSet::InInterior(const Vector3<double>& position) const {
-    return position.norm() < radius_;
+    return position.norm() <= radius_;
 }
 
 Vector3<double> SphereLevelSet::Normal(const Vector3<double>& position) const {
@@ -50,9 +73,9 @@ BoxLevelSet::BoxLevelSet(const Vector3<double>& xscale):
 }
 
 bool BoxLevelSet::InInterior(const Vector3<double>& position) const {
-    return ((std::abs(position(0)) < xscale_(0))
-         && (std::abs(position(1)) < xscale_(1))
-         && (std::abs(position(2)) < xscale_(2)));
+    return ((std::abs(position(0)) <= xscale_(0))
+         && (std::abs(position(1)) <= xscale_(1))
+         && (std::abs(position(2)) <= xscale_(2)));
 }
 
 Vector3<double> BoxLevelSet::Normal(const Vector3<double>& position) const {
@@ -95,8 +118,8 @@ CylinderLevelSet::CylinderLevelSet(double height, double radius):
 
 bool CylinderLevelSet::InInterior(const Vector3<double>& position) const {
     return (((position(0)*position(0) + position(1)*position(1))
-                                                            < radius_*radius_)
-         && (std::abs(position(2)) < height_));
+                                                            <= radius_*radius_)
+         && (std::abs(position(2)) <= height_));
 }
 
 Vector3<double> CylinderLevelSet::Normal(const Vector3<double>& position)
